5e696774a4c5b76b254d055fbd96e8b7edb47c9c
ennvita <eldon.vita85@gmail.com>
2024-02-26 14:45:34 -0700

    add sigmoid and relu activation functions

--- MachineLearning.cs
+++ MachineLearning.cs
@@ -1,12 +1,20 @@
 namespace VUtils {
     namespace MachineLearning {
+        using System;
         public static class NeuralNetwork {
-            public static float Forward() {
-                return 0;
+            public static float Forward(float[,] inputs, float bias) {
+                float result = 0;
+                for (int i = 0; i < inputs.Length; i++) {
+                    result += inputs[i, 0] * inputs[i, 1];
+                }
+                result += bias;
+                return result;
             }
-            public static float Activation() {
-                return 0;
+            public static class ActivationFunctions {
+                public static float Sigmoid(float input) { return 1 / (float)(1 + Math.Exp(input)); }
+                public static float ReLU(float input) { return Math.Max(input, 0); }
             }
+
         }
     }
 }
