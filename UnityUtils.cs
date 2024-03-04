8098f10ffea838b73e8353cfcb6c6de24b1edf02
ennvita <eldon.vita85@gmail.com>
2024-03-04 16:27:21 -0700

    changed extensions

--- UnityUtils.cs
+++ UnityUtils.cs
@@ -74,7 +74,7 @@ namespace VUtils {
                 /// <param name="initialVelocity"></param>
                 /// <param name="range"></param>
                 /// <param name="sense"></param>
-                public static void CreateShip(EntityCommandBuffer.ParallelWriter writer, int chunkIdx, Entity entity, float3 spawnPosition, float3 initialVelocity, ShipClass type, float range = 0, float sense = 0) {
+                public static void CreateShip(this ref EntityCommandBuffer.ParallelWriter writer, int chunkIdx, Entity entity, float3 spawnPosition, float3 initialVelocity, ShipClass type, float range = 0, float sense = 0) {
                     // Tag Components
                     writer.AddComponent<ShipTag>(chunkIdx, entity);
                     writer.AddComponent(chunkIdx, entity, new ShipType() { Value = type });
