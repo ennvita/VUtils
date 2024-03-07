d334dae5fadbbb07dfecb28e99852f4f94f1e9b3
ennvita <eldon.vita85@gmail.com>
2024-03-07 13:01:34 -0700

    update forward function

--- MachineLearning.cs
+++ MachineLearning.cs
@@ -15,10 +15,9 @@ namespace VUtils {
                 return input * bias + weight;
             }
             public static void Forward(DynamicBuffer<Interaction> inters) {
-                DynamicBuffer<Interaction> tmp = new();
                 for (int i = 0; i < inters.Length; i++) {
                     var output = Forward(inters[i].Input.Value, inters[i].Output.Bias, inters[i].Weight);
-                    tmp[i] = new Interaction {
+                    inters[i] = new Interaction {
                         ID = inters[i].ID,
                         Input = inters[i].Input,
                         Output = new Node {
@@ -29,7 +28,6 @@ namespace VUtils {
                         }
                     };
                 }
-                inters.CopyFrom(tmp);
             }
             public static class ActivationFunctions {
                 public static float Sigmoid(float input) { return 1 / (float)(1 + Math.Exp(input)); }
